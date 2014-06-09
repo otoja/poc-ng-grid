f1d11c05a0bfe8286c078dff9565ec86509d23c8
Kama <shagrin84@gmail.com>
2014-06-09 14:02:18 +0200

    Pagination

--- poc-ng-grid/public_html/js/controllers.js
+++ poc-ng-grid/public_html/js/controllers.js
@@ -4,21 +4,72 @@
 
 angular.module('myApp.controllers', ['ngGrid'])
         .controller('MyCtrl1', ['$scope', '$http', function($scope, $http) {
-                $http.get('data/data.json').success(function(allData) {
-                    $scope.myData = allData;
-                });
+                //configure grid
                 $scope.filterOptions = {
-                    filterText: '',
-                    useExternalFilter: true
+                    filterText: ''
                 };
+
+                $scope.pagingOptions = {
+                    currentPage: 1,
+                    pageSizes: [10, 20, 50, 100, 200],
+                    pageSize: 50
+                }
+
+                //pagination functions
+                $scope.setPagingData = function(data, page, pageSize) {
+                    var pagedData = data.slice((page - 1) * pageSize, page * pageSize);
+                    $scope.myData = pagedData;
+                    $scope.totalServerItems = data.length;
+                    if (!$scope.$$phase) {
+                        $scope.$apply();
+                    }
+                };
+                $scope.getPagedDataAsync = function(pageSize, page, searchText) {
+                    var data;
+                    if (searchText) {
+                        var ft = searchText.toLowerCase();
+                        $http.get('data/data.json').success(function(allData) {
+                            data = allData.filter(function(item) {
+                                return JSON.stringify(item).toLowerCase().indexOf(ft) != -1;
+                            });
+                            $scope.setPagingData(data, page, pageSize);
+                        });
+                    } else {
+                        $http.get('data/data.json').success(function(allData) {
+                            $scope.setPagingData(allData, page, pageSize);
+                        });
+                    }
+                };
+
+                $scope.getPagedDataAsync($scope.pagingOptions.pageSize, $scope.pagingOptions.currentPage);
+
+                $scope.$watch('pagingOptions', function(newVal, oldVal) {
+                    if (newVal !== oldVal) {
+                        $scope.getPagedDataAsync($scope.pagingOptions.pageSize, $scope.pagingOptions.currentPage, $scope.filterOptions.filterText);
+                    }
+                }, true);
+
+
                 $scope.gridOptions = {
-                    data: 'myData',
+                    primaryKey: 'guid',
+                    data: 'myData', //data set
+
+                    //selection config
                     highlighting: true,
                     showSelectionCheckbox: true,
                     multiSelect: true,
                     selectWithCheckboxOnly: true,
-                    primaryKey: 'guid',
-                    filterOptions: $scope.filterOptions
-//                    plugins: [new ngGridFlexibleHeightPlugin()]
+                    
+                    //filtering
+                    filterOptions: $scope.filterOptions,
+                    
+                    //pagination
+                    enablePaging: true,
+                    pagingOptions: $scope.pagingOptions,
+                    totalServerItems: 'totalServerItems',
+                    showFooter: true,
+                    
+                    //plugins
+                    plugins: [new ngGridFlexibleHeightPlugin()]
                 };
             }]);
